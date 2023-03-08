ed4218d6bc420b8f1eec6934c387675db1198c04
Rohit Sharma <116329870+Rohit04Sharma01@users.noreply.github.com>
2023-03-08 20:28:36 +0530

    Added node

--- node.py
+++ node.py
@@ -1,11 +1,19 @@
+from operations import *
+
 class Node:
-    def __init__(self, value, x, y):
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-        self.x_pos = x
-        self.y_pos = y
+        self.x_pos = None
+        self.y_pos = None
+        give_coordinates_to_node(self)
+
     
     def get_coordinates(self):
         co_arr = (self.x_pos, self.y_pos)
         return co_arr
+    
+
+n = Node(10)
+print(n.get_coordinates())
--- operations.py
+++ operations.py
@@ -1,2 +1,31 @@
 from node import Node
 
+TOTAL_FRAME_WIDTH = 950
+TOTAL_FRAME_HEIGHT = 655
+
+node_list = []
+root = None
+
+def get_prev_node(n: Node, val: int, pos: list):
+    if n.left == None and n.right == None:
+        pos.append(n.x_pos)
+        pos.append(n.y_pos)
+        return
+    
+    if n.value > val:
+        get_prev_node(n.left, val)
+    else:
+        get_prev_node(n.right, val)
+    pass
+
+def give_coordinates_to_node(n: Node):
+    if not len(node_list):
+        root = n
+        n.x_pos = TOTAL_FRAME_WIDTH // 2
+        n.y_pos = TOTAL_FRAME_HEIGHT // 2
+    else:
+        pos = []
+        get_prev_node(root, n.value, pos)
+        n.x_pos = pos[0] + 50
+        n.y_pos = pos[1] + 50
+    pass
