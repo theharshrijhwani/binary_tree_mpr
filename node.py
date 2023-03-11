b4766c7d965d4003c3b998289629debfddf49761
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2023-03-11 12:55:45 +0530

    added comments

--- node.py
+++ node.py
@@ -8,9 +8,9 @@ class Node:
     
 
 FRAME_COORDINATE = [950, 655] 
-root = None
+root = None 
 
-binary_tree = []
+binary_tree = []  # all the nodes will be stored here
 
 def search_correct_pos(val:int, prev:Node, node: Node):
     if node == None:
@@ -22,15 +22,15 @@ def search_correct_pos(val:int, prev:Node, node: Node):
 
 
 def give_coordinates(n: Node):
-    global root
+    global root # using root which is global
     val = n.val
-    pos = []
+    # pos = []
     if not len(binary_tree):
         root = n
         n.x_pos = FRAME_COORDINATE[0]//2
         n.y_pos = 100
     else:
-        return_node = search_correct_pos(val, None, root)
+        return_node = search_correct_pos(val, None, root) # the return_node is first assigned with root node and then it gradually traverses
         if return_node.val < val:
             return_node.right = n
             n.x_pos = return_node.x_pos + 50
@@ -43,18 +43,18 @@ def give_coordinates(n: Node):
     binary_tree.append(n)
 
 
-n1 = Node(10)
-n2 = Node(50)
-n3 = Node(5)
-n4 = Node(100)
-n5 = Node(75)
+# n1 = Node(10)
+# n2 = Node(50)
+# n3 = Node(5)
+# n4 = Node(100)
+# n5 = Node(75)
 
 
-give_coordinates(n1)
-give_coordinates(n2)
-give_coordinates(n3)
-give_coordinates(n4)
-give_coordinates(n5)
+# give_coordinates(n1)
+# give_coordinates(n2)
+# give_coordinates(n3)
+# give_coordinates(n4)
+# give_coordinates(n5)
 
 for i in binary_tree:
     print(i.x_pos, i.y_pos)
