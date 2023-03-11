288cf8d92bd218bc6b260688cf0fcbe2e8057b52
Rohit Sharma <116329870+Rohit04Sharma01@users.noreply.github.com>
2023-03-11 11:56:53 +0530

    Update node.py

--- node.py
+++ node.py
@@ -1,19 +1,60 @@
-from operations import *
-
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, _val):
+        self.val = _val
         self.left = None
         self.right = None
         self.x_pos = None
         self.y_pos = None
-        give_coordinates_to_node(self)
-
-    
-    def get_coordinates(self):
-        co_arr = (self.x_pos, self.y_pos)
-        return co_arr
     
 
-n = Node(10)
-print(n.get_coordinates())
+FRAME_COORDINATE = [950, 655] 
+root = None
+
+binary_tree = []
+
+def search_correct_pos(val:int, prev:Node, node: Node):
+    if node == None:
+        return prev
+    if val < node.val:
+        return search_correct_pos(val, node, node.left)
+    else:
+        return search_correct_pos(val, node, node.right)
+
+
+def give_coordinates(n: Node):
+    global root
+    val = n.val
+    pos = []
+    if not len(binary_tree):
+        root = n
+        n.x_pos = FRAME_COORDINATE[0]//2
+        n.y_pos = 100
+    else:
+        return_node = search_correct_pos(val, None, root)
+        if return_node.val < val:
+            return_node.right = n
+            n.x_pos = return_node.x_pos + 50
+            n.y_pos = return_node.y_pos + 50
+        else:
+            return_node.left = n
+            n.x_pos = return_node.x_pos - 50
+            n.y_pos = return_node.y_pos + 50
+        
+    binary_tree.append(n)
+
+
+n1 = Node(10)
+n2 = Node(50)
+n3 = Node(5)
+n4 = Node(100)
+n5 = Node(75)
+
+
+give_coordinates(n1)
+give_coordinates(n2)
+give_coordinates(n3)
+give_coordinates(n4)
+give_coordinates(n5)
+
+for i in binary_tree:
+    print(i.x_pos, i.y_pos)
